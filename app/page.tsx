2be1b837af0a9f2899fa323cccc3025270ee5e4b
ZainAli909192 <malikzain909192@gmail.com>
2026-08-21 17:17:53 +0400

    Comming soon code updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,25 @@
 export default function Home() {
   return (
-    <main className="fixed inset-0 h-[100dvh] w-screen overflow-hidden bg-black">
+    <main className="fixed inset-0 h-[100dvh] w-screen overflow-hidden bg-[#f3e7d8]">
+      
       {/* Desktop */}
       <div
-        className="hidden h-full w-full bg-cover bg-center bg-no-repeat lg:block"
+        className="hidden h-full w-full bg-center bg-no-repeat lg:block"
         style={{
           backgroundImage: "url('/commingsoon.png')",
+          backgroundSize: "contain",
         }}
       />
 
       {/* Mobile + Tablet */}
       <div
-        className="block h-full w-full bg-no-repeat lg:hidden"
+        className="block h-full w-full bg-center bg-no-repeat lg:hidden"
         style={{
           backgroundImage: "url('/mobilecommingsoon.png')",
-          backgroundSize: "cover",
-          backgroundPosition: "center top",
+          backgroundSize: "contain",
         }}
       />
+      
     </main>
   );
 }
